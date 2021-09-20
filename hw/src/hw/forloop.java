6e49b4b0077c2ee69145bb1ff9d8a047cd2c63b2
sarwarali847 <sarwarali847@gmail.com>
2021-09-20 12:28:29 +0530

    Again added new file

---
+++ hw/src/hw/forloop.java
@@ -0,0 +1,13 @@
+package hw;
+
+public class forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i=1;i<=10;i++)
+			System.out.println(i);
+
+	}
+
+}
