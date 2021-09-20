d73533348f6761d183a00c22b7581b4f997bca7a
sarwarali847 <sarwarali847@gmail.com>
2021-09-20 12:22:28 +0530

    Added New File

---
+++ hw/src/hw/square.java
@@ -0,0 +1,18 @@
+package hw;
+import java.util.*;
+
+public class square {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		double a=sc.nextInt();
+		double b=sc.nextInt();
+		double c;
+		c=Math.pow(a, b);
+		System.out.println(c);
+		
+
+	}
+
+}
